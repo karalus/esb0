4033e9dbb1b56bfad38eef8956733ba354b903d2
Andre Karalus <andre.karalus@karalus-it.de>
2019-01-02 00:22:44 +0100

    allow for reuse of reestablished http connection

--- src/main/java/com/artofarc/esb/http/HttpUrlSelector.java
+++ src/main/java/com/artofarc/esb/http/HttpUrlSelector.java
@@ -24,6 +24,7 @@ import java.util.Map.Entry;
 import java.util.Set;
 import java.util.concurrent.ScheduledFuture;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicIntegerArray;
 
 import javax.management.AttributeChangeNotification;
 import javax.management.MBeanNotificationInfo;
@@ -45,7 +46,7 @@ public final class HttpUrlSelector extends NotificationBroadcasterSupport implem
 		private HttpUrlConnectionWrapper(int pos, HttpURLConnection httpURLConnection) {
 			_pos = pos;
 			_httpURLConnection = httpURLConnection;
-			setInUse(pos, true);
+			inUse.incrementAndGet(pos);
 		}
 
 		public HttpURLConnection getHttpURLConnection() {
@@ -53,7 +54,7 @@ public final class HttpUrlSelector extends NotificationBroadcasterSupport implem
 		}
 
 		public void close() {
-			setInUse(_pos, false);
+			inUse.decrementAndGet(_pos);
 		}
 	}
 	
@@ -64,11 +65,11 @@ public final class HttpUrlSelector extends NotificationBroadcasterSupport implem
 	private final int size;
 	private final int[] weight;
 	private final boolean[] active;
-	private final boolean[] inUse;
+	private final AtomicIntegerArray inUse;
 
 	private int pos;
 	private volatile int activeCount;
-	private volatile long sequenceNumber;
+	private long sequenceNumber;
 
 	private ScheduledFuture<?> _future;
 
@@ -81,7 +82,7 @@ public final class HttpUrlSelector extends NotificationBroadcasterSupport implem
 		size = httpEndpoint.getHttpUrls().size();
 		weight = new int[size];
 		active = new boolean[size];
-		inUse = new boolean[size];
+		inUse = new AtomicIntegerArray(size);
 		for (int i = 0; i < size; ++i) {
 			weight[i] = httpEndpoint.getHttpUrls().get(i).getWeight();
 			boolean isActive = httpEndpoint.getHttpUrls().get(i).isActive();
@@ -121,10 +122,6 @@ public final class HttpUrlSelector extends NotificationBroadcasterSupport implem
 		}
 	}
 
-	public synchronized void setInUse(int pos, boolean b) {
-		inUse[pos] = b;
-	}
-	
 	public synchronized void stop() {
 		if (_future != null) {
 			_future.cancel(true);
@@ -139,7 +136,8 @@ public final class HttpUrlSelector extends NotificationBroadcasterSupport implem
 				try {
 					HttpURLConnection conn = (HttpURLConnection) _httpEndpoint.getHttpUrls().get(i).getUrl().openConnection();
 					conn.setConnectTimeout(_httpEndpoint.getConnectionTimeout());
-					conn.connect();
+					conn.setRequestMethod("HEAD");
+					conn.getResponseCode();
 					setActive(i, true);
 				} catch (IOException e) {
 					// ignore
@@ -190,6 +188,7 @@ public final class HttpUrlSelector extends NotificationBroadcasterSupport implem
 		}
 		HttpURLConnection conn = (HttpURLConnection) url.openConnection();
 		conn.setConnectTimeout(_httpEndpoint.getConnectionTimeout());
+		// For "PATCH" refer to https://stackoverflow.com/questions/25163131/httpurlconnection-invalid-http-method-patch
 		conn.setRequestMethod(method);
 		conn.setDoOutput(true);
 		conn.setInstanceFollowRedirects(false);
@@ -207,12 +206,12 @@ public final class HttpUrlSelector extends NotificationBroadcasterSupport implem
 
 	public CompositeDataSupport[] getHttpEndpointStates() throws OpenDataException {
 		String[] itemNames = new String[] { "URL", "weight", "active", "inUse" };
-		OpenType<?>[] itemTypes = new OpenType[] { SimpleType.STRING, SimpleType.INTEGER, SimpleType.BOOLEAN, SimpleType.BOOLEAN };
+		OpenType<?>[] itemTypes = new OpenType[] { SimpleType.STRING, SimpleType.INTEGER, SimpleType.BOOLEAN, SimpleType.INTEGER };
 		CompositeType rowType = new CompositeType("HttpEndpointState", "State of HttpEndpoint", itemNames, itemNames, itemTypes);
 
 		CompositeDataSupport[] result = new CompositeDataSupport[size];
 		for (int i = 0; i < size; ++i) {
-			Object[] itemValues = new Object[] { _httpEndpoint.getHttpUrls().get(i).getBaseURL(), weight[i], isActive(i), inUse[i] };
+			Object[] itemValues = new Object[] { _httpEndpoint.getHttpUrls().get(i).getBaseURL(), weight[i], isActive(i), inUse.get(i) };
 			result[i] = new CompositeDataSupport(rowType, itemNames, itemValues);
 		}
 		return result;
