8bc84e85194ec0ff8a43fdf62be199ff2ccd05da
Andre Karalus <andre.karalus@karalus-it.de>
2019-02-07 00:39:45 +0100

    FileSystemWatchAction: delete file after processing

--- src/main/java/com/artofarc/esb/action/FileSystemWatchAction.java
+++ src/main/java/com/artofarc/esb/action/FileSystemWatchAction.java
@@ -18,10 +18,12 @@ package com.artofarc.esb.action;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.nio.channels.Channels;
+import java.nio.channels.FileChannel;
 import java.nio.file.FileSystems;
-import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
 import java.nio.file.StandardWatchEventKinds;
 import java.nio.file.WatchEvent;
 import java.nio.file.WatchKey;
@@ -37,10 +39,12 @@ import java.util.logging.Level;
 import com.artofarc.esb.context.Context;
 import com.artofarc.esb.context.ExecutionContext;
 import com.artofarc.esb.message.BodyType;
-import com.artofarc.esb.message.ESBMessage;
 import com.artofarc.esb.message.ESBConstants;
+import com.artofarc.esb.message.ESBMessage;
 
 public class FileSystemWatchAction extends TerminalAction {
+
+	private static final String INPUT_STREAM = "InputStream";
 
 	private final long _timeout;
 	private final String _workerPool;
@@ -54,7 +58,7 @@ public class FileSystemWatchAction extends TerminalAction {
 		_watchService = FileSystems.getDefault().newWatchService();
 		for (String dir : dirs) {
 			Path path = Paths.get(dir);
-			path.register(_watchService, StandardWatchEventKinds.ENTRY_MODIFY, StandardWatchEventKinds.ENTRY_CREATE);
+			path.register(_watchService, StandardWatchEventKinds.ENTRY_CREATE);
 		}
 	}
 
@@ -67,26 +71,32 @@ public class FileSystemWatchAction extends TerminalAction {
 				Path path = (Path) watchEvent.context();
 				Path parent = (Path) watchKey.watchable();
 				Path absolutePath = parent.resolve(path);
-				final ESBMessage msg = new ESBMessage(BodyType.INPUT_STREAM, Files.newInputStream(absolutePath));
-				msg.getVariables().put("InputStream", msg.getBody());
-				msg.getVariables().put(ESBConstants.HttpMethod, watchEvent.kind().toString());
-				msg.getVariables().put(ESBConstants.ContextPath, parent.toString());
-				msg.getVariables().put(ESBConstants.PathInfo, path.toString());
-				logger.fine("Absolute path " + absolutePath + ", kind: " + watchEvent.kind());
-				for(;;) {
-					try {
-						futures.put(SpawnAction.submit(context, msg, _workerPool, _spawn, Collections.<Action>emptyList(), true), watchEvent);
-						break;
-					} catch (RejectedExecutionException e) {
-						logger.warning("Could not spawn to worker pool " + _workerPool);
-						Thread.sleep(100L);
+				FileChannel fileChannel = FileChannel.open(absolutePath, StandardOpenOption.READ, StandardOpenOption.WRITE, StandardOpenOption.DELETE_ON_CLOSE);
+				if (fileChannel.tryLock() != null) {
+					ESBMessage msg = new ESBMessage(BodyType.INPUT_STREAM, Channels.newInputStream(fileChannel));
+					msg.getVariables().put(INPUT_STREAM, msg.getBody());
+					msg.getVariables().put(ESBConstants.HttpMethod, watchEvent.kind().toString());
+					msg.getVariables().put(ESBConstants.ContextPath, parent.toString());
+					msg.getVariables().put(ESBConstants.PathInfo, path.toString());
+					logger.fine("Absolute path " + absolutePath + ", kind: " + watchEvent.kind());
+					for(;;) {
+						try {
+							futures.put(SpawnAction.submit(context, msg, _workerPool, _spawn, Collections.<Action> emptyList(), true), watchEvent);
+							break;
+						} catch (RejectedExecutionException e) {
+							logger.warning("Could not spawn to worker pool " + _workerPool);
+							Thread.sleep(100L);
+						}
 					}
+				} else {
+					logger.warning("File could not be locked " + absolutePath);
+					fileChannel.close();
 				}
 			}
 			watchKey.reset();
 			for (Future<ESBMessage> future : futures.keySet()) {
 				try {
-					InputStream is = SpawnAction.join(context, message, future).getVariable("InputStream");
+					InputStream is = SpawnAction.join(context, message, future).getVariable(INPUT_STREAM);
 					is.close();
 				} catch (Exception e) {
 					WatchEvent<?> watchEvent = futures.get(future);
--- src/test/java/com/artofarc/esb/action/FileSystemWatchTest.java
+++ src/test/java/com/artofarc/esb/action/FileSystemWatchTest.java
@@ -13,55 +13,53 @@ import org.junit.Test;
 import com.artofarc.esb.AbstractESBTest;
 import com.artofarc.esb.TimerService;
 
-
 public class FileSystemWatchTest extends AbstractESBTest {
-   
-   @Test
-   public void testTimerService() throws Exception {
-      TimerService timerService = new TimerService(null, null, 0, 100, false);
-      MarkAction markAction = new MarkAction();
-      timerService.setStartAction(markAction);
-      timerService.init(context.getPoolContext().getGlobalContext());
-      timerService.enable(true);
-      assertFalse(markAction.isExecuted());
-      Thread.sleep(100);
-      timerService.enable(false);
-      assertTrue(markAction.isExecuted());
-   }
 
-   @Test
-   public void testDetectChange() throws Exception {
-   	Path tempDirectory = Files.createTempDirectory("JUnit-");
-   	File dir = tempDirectory.toFile();
-   	dir.deleteOnExit();
-   	File inDir = new File(dir, "in");
-   	inDir.mkdir();
-   	inDir.deleteOnExit();
-   	File outDir = new File(dir, "out");
-   	outDir.mkdir();
-   	outDir.deleteOnExit();
-   	
-      TimerService timerService = new TimerService(null, null, 0, 1, false);
+	@Test
+	public void testTimerService() throws Exception {
+		TimerService timerService = new TimerService(null, null, 0, 100, false);
+		MarkAction markAction = new MarkAction();
+		timerService.setStartAction(markAction);
+		timerService.init(context.getPoolContext().getGlobalContext());
+		timerService.enable(true);
+		assertFalse(markAction.isExecuted());
+		Thread.sleep(100);
+		timerService.enable(false);
+		assertTrue(markAction.isExecuted());
+	}
+
+	@Test
+	public void testDetectChange() throws Exception {
+		Path tempDirectory = Files.createTempDirectory("JUnit-");
+		File dir = tempDirectory.toFile();
+		dir.deleteOnExit();
+		File inDir = new File(dir, "in");
+		inDir.mkdir();
+		inDir.deleteOnExit();
+		File outDir = new File(dir, "out");
+		outDir.mkdir();
+		outDir.deleteOnExit();
+		TimerService timerService = new TimerService(null, null, 0, 1, false);
 		FileSystemWatchAction action = new FileSystemWatchAction(Arrays.asList(new String[] { inDir.getPath() }), 90l, null, new FileAction(outDir.getPath()));
-      timerService.setStartAction(action);
-      timerService.init(context.getPoolContext().getGlobalContext());
-      timerService.enable(true);
-      File outFile1 = new File(outDir, "test1");
-      File outFile2 = new File(outDir, "test2");
-      assertFalse(outFile1.exists());
-      File inFile1 = new File(inDir, "test1");
+		timerService.setStartAction(action);
+		timerService.init(context.getPoolContext().getGlobalContext());
+		timerService.enable(true);
+		File outFile1 = new File(outDir, "test1");
+		File outFile2 = new File(outDir, "test2");
+		assertFalse(outFile1.exists());
+		File inFile1 = new File(inDir, "test1");
 		new FileOutputStream(inFile1).close();
-      Thread.sleep(10);
-      File inFile2 = new File(inDir, "test2");
+		Thread.sleep(10);
+		File inFile2 = new File(inDir, "test2");
 		new FileOutputStream(inFile2).close();
-      Thread.sleep(1100);
-		inFile1.deleteOnExit();
-		inFile2.deleteOnExit();
-      assertTrue(outFile1.exists());
-      outFile1.deleteOnExit();
-      assertTrue(outFile2.exists());
-      outFile2.deleteOnExit();
-      timerService.enable(false);
-   }
-   
+		Thread.sleep(1100);
+		assertTrue(outFile1.exists());
+		assertFalse(inFile1.exists());
+		outFile1.deleteOnExit();
+		assertTrue(outFile2.exists());
+		assertFalse(inFile2.exists());
+		outFile2.deleteOnExit();
+		timerService.enable(false);
+	}
+
 }
