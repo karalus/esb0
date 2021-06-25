02d25b0dd13599a6292b90eb0bc4b82fa4abdfde
Andre Karalus <andre@karalus-it.de>
2021-06-25 19:01:04 +0200

    Bugfix: "text/plain" could not be used in XQuery

--- src/main/java/com/artofarc/esb/action/TransformAction.java
+++ src/main/java/com/artofarc/esb/action/TransformAction.java
@@ -106,7 +106,7 @@ public class TransformAction extends Action {
 				xqExpression.bindString(XQConstants.CONTEXT_ITEM, "", null);
 			} else {
 				String contentType = message.getHeader(HttpConstants.HTTP_HEADER_CONTENT_TYPE);
-				if (contentType != null && contentType.startsWith(HttpConstants.HTTP_HEADER_CONTENT_TYPE_JSON)) {
+				if (HttpConstants.isNotSOAP(contentType)) {
 					xqExpression.bindString(XQConstants.CONTEXT_ITEM, message.getBodyAsString(context), null);
 				} else {
 					switch (message.getBodyType()) {
