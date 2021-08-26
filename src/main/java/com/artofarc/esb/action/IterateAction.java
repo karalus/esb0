f841dc3473711cd1a5ee76cf0400ed0c4fe7139b
Andre Karalus <andre@karalus-it.de>
2021-08-26 23:46:30 +0200

    Feature IterateAction: run variable can be message body

--- src/main/java/com/artofarc/esb/action/IterateAction.java
+++ src/main/java/com/artofarc/esb/action/IterateAction.java
@@ -1,12 +1,11 @@
 /*
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements. See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
- * the License. You may obtain a copy of the License at
+ * Copyright 2021 Andre Karalus
  *
- *    http://www.apache.org/licenses/LICENSE-2.0
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -55,7 +54,11 @@ public class IterateAction extends Action {
 	protected void execute(Context context, ExecutionContext execContext, ESBMessage message, boolean nextActionIsPipelineStop) {
 		Iterator<?> iterator = execContext.getResource();
 		if (iterator.hasNext()) {
-			message.putVariable(_varName, iterator.next());
+			if ("body".equals(_varName)) {
+				message.reset(null, iterator.next());
+			} else {
+				message.putVariable(_varName, iterator.next());
+			}
 			if (_remove) {
 				iterator.remove();
 			}
