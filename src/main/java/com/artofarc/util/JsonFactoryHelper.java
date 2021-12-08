9e798c1577d03e5ff181fccd78cf034f737f3d94
Andre Karalus <andre@karalus-it.de>
2021-12-08 22:39:45 +0100

    Performance: Alternate BufferPool for excessive Json processing

--- src/main/java/com/artofarc/util/JsonFactoryHelper.java
+++ src/main/java/com/artofarc/util/JsonFactoryHelper.java
@@ -1,12 +1,11 @@
 /*
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements. See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
- * the License. You may obtain a copy of the License at
+ * Copyright 2021 Andre Karalus
  *
- *    http://www.apache.org/licenses/LICENSE-2.0
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -26,16 +25,34 @@ import javax.json.stream.JsonGenerator;
 import javax.json.stream.JsonGeneratorFactory;
 import javax.json.stream.JsonParserFactory;
 
+import org.glassfish.json.api.BufferPool;
+
 public final class JsonFactoryHelper {
 
-	public static final JsonReaderFactory JSON_READER_FACTORY = Json.createReaderFactory(null);
-	public static final JsonParserFactory JSON_PARSER_FACTORY = Json.createParserFactory(null);
+	public static final JsonReaderFactory JSON_READER_FACTORY;
+	public static final JsonParserFactory JSON_PARSER_FACTORY;
 	public static final JsonGeneratorFactory JSON_GENERATOR_FACTORY;
-	public static final JsonBuilderFactory JSON_BUILDER_FACTORY = Json.createBuilderFactory(null);
+	public static final JsonBuilderFactory JSON_BUILDER_FACTORY;
 	public static final JsonWriterFactory JSON_WRITER_FACTORY;
 
 	static {
 		HashMap<String, Object> config = new HashMap<>();
+		if (Boolean.parseBoolean(System.getProperty("esb0.jsonGlassfishAlternateBufferPool"))) {
+			config.put(BufferPool.class.getName(), new BufferPool() {
+
+				@Override
+				public char[] take() {
+					return new char[IOUtils.MTU];
+				}
+
+				@Override
+				public void recycle(char[] buf) {
+				}
+			});
+		}
+		JSON_READER_FACTORY = Json.createReaderFactory(config);
+		JSON_PARSER_FACTORY = Json.createParserFactory(config);
+		JSON_BUILDER_FACTORY = Json.createBuilderFactory(config);
 		if (Boolean.parseBoolean(System.getProperty("esb0.jsonPrettyPrinting", "true"))) {
 			config.put(JsonGenerator.PRETTY_PRINTING, true);
 		}
