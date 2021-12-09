c5bc8180fbb18c14759866a338f72541761f2cb1
Andre Karalus <andre@karalus-it.de>
2021-12-09 22:03:30 +0100

    Clean API: Don't publish private class as return type

--- src/main/java/com/artofarc/esb/resource/LRUCacheWithExpirationFactory.java
+++ src/main/java/com/artofarc/esb/resource/LRUCacheWithExpirationFactory.java
@@ -1,12 +1,11 @@
 /*
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements. See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
- * the License. You may obtain a copy of the License at
+ * Copyright 2021 Andre Karalus
  *
- *    http://www.apache.org/licenses/LICENSE-2.0
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -20,6 +19,7 @@ import java.util.Collection;
 import java.util.Collections;
 import java.util.LinkedHashMap;
 import java.util.Map;
+import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.DelayQueue;
 import java.util.concurrent.Delayed;
@@ -113,7 +113,11 @@ public class LRUCacheWithExpirationFactory<K, V> extends ResourceFactory<LRUCach
 			return _cache.remove(key);
 		}
 
-		public Collection<Expiration<K>> getExpirations() {
+		public Set<K> keySet() {
+			return _expirationKeys.keySet();
+		}
+
+		public Collection<?> getExpirations() {
 			return _expirationKeys.values();
 		}
 
