92bec13373dd56bd3ea1c5c06ed6788b9ca4d178
Andre Karalus <andre@karalus-it.de>
2022-01-07 18:11:00 +0100

    Feature: BranchOnVariableAction better handling of java.util.Optional

--- src/main/java/com/artofarc/esb/action/BranchOnVariableAction.java
+++ src/main/java/com/artofarc/esb/action/BranchOnVariableAction.java
@@ -1,12 +1,11 @@
 /*
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements. See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
- * the License. You may obtain a copy of the License at
+ * Copyright 2021 Andre Karalus
  *
- *    http://www.apache.org/licenses/LICENSE-2.0
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -19,6 +18,7 @@ package com.artofarc.esb.action;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -84,6 +84,9 @@ public class BranchOnVariableAction extends Action {
 	@Override
 	protected ExecutionContext prepare(Context context, ESBMessage message, boolean inPipeline) throws Exception {
 		Object value = _varName == "body" ? message.getBody() != null ? message.getBodyAsString(context) : null : resolve(message, _varName, true);
+		if (value instanceof Optional<?>) {
+			value = ((Optional<?>) value).orElse(null);
+		}
 		Action action = _nullAction;
 		if (value != null) {
 			action = _defaultAction;
