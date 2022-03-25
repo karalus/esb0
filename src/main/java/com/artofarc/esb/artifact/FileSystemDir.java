f4e3cd3ad0bb1dfbde2a349f403bb1110e7d5966
Andre Karalus <andre@karalus-it.de>
2022-03-25 14:42:33 +0100

    Eat your own dog food

--- src/main/java/com/artofarc/esb/artifact/FileSystemDir.java
+++ src/main/java/com/artofarc/esb/artifact/FileSystemDir.java
@@ -1,12 +1,11 @@
 /*
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements. See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
- * the License. You may obtain a copy of the License at
+ * Copyright 2022 Andre Karalus
  *
- *    http://www.apache.org/licenses/LICENSE-2.0
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -17,7 +16,6 @@
 package com.artofarc.esb.artifact;
 
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -47,7 +45,7 @@ public class FileSystemDir extends FileSystem {
 
 	@Override
 	protected InputStream createInputStream(String uri) throws FileNotFoundException {
-		return new FileInputStream(new File(_anchorDir, uri));
+		return new IOUtils.PredictableFileInputStream(new File(_anchorDir, uri));
 	}
 
 	@Override
