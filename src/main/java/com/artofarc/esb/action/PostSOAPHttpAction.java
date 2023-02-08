9042542b0ef06c50cbfa4c14489faeb042acb5ab
Andre Karalus <andre@karalus-it.de>
2023-02-08 11:01:08 +0100

    Check HTTP status code for SOAP 1.1 compliance

--- src/main/java/com/artofarc/esb/action/PostSOAPHttpAction.java
+++ src/main/java/com/artofarc/esb/action/PostSOAPHttpAction.java
@@ -1,12 +1,11 @@
 /*
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements. See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
- * the License. You may obtain a copy of the License at
+ * Copyright 2023 Andre Karalus
  *
- *    http://www.apache.org/licenses/LICENSE-2.0
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -16,7 +15,7 @@
  */
 package com.artofarc.esb.action;
 
-import java.net.HttpURLConnection;
+import static java.net.HttpURLConnection.*;
 
 import com.artofarc.esb.context.Context;
 import com.artofarc.esb.context.ExecutionContext;
@@ -33,9 +32,13 @@ public class PostSOAPHttpAction extends UnwrapSOAPAction {
 	protected ExecutionContext prepare(Context context, ESBMessage message, boolean inPipeline) throws Exception {
 		Integer httpResponseCode = message.getVariable(ESBConstants.HttpResponseCode);
 		switch (httpResponseCode) {
-		case HttpURLConnection.HTTP_OK:
-		case HttpURLConnection.HTTP_ACCEPTED:
-		case HttpURLConnection.HTTP_INTERNAL_ERROR:
+		case HTTP_ACCEPTED:
+			if (!_soap12) {
+				throw new ExecutionException(this, "HTTP Response Code 202 not covered by SOAP 1.1 protocol");
+			}
+			// no break
+		case HTTP_OK:
+		case HTTP_INTERNAL_ERROR:
 			return super.prepare(context, message, inPipeline);
 		default:
 			throw new ExecutionException(this, "HTTP Response Code not covered by SOAP protocol, was " + httpResponseCode);
