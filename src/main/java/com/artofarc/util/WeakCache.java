18cff86054dbe66f72fba7cea5acdaa327933434
Andre Karalus <andre@karalus-it.de>
2023-04-19 22:54:46 +0200

    Bugfix: NPE under high memory pressure

--- src/main/java/com/artofarc/util/WeakCache.java
+++ src/main/java/com/artofarc/util/WeakCache.java
@@ -1,12 +1,11 @@
 /*
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements. See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
- * the License. You may obtain a copy of the License at
+ * Copyright 2023 Andre Karalus
  *
- *    http://www.apache.org/licenses/LICENSE-2.0
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -38,11 +37,18 @@ public abstract class WeakCache<K, V> {
 			}
 		}
 		ref = cache.get(key);
+		V value;
 		if (ref == null) {
-			ref = new WeakReference<>(create(key), queue);
-			cache.put(key, ref);
+			value = create(key);
+			cache.put(key, new WeakReference<>(value, queue));
+		} else {
+			value = ref.get();
+			if (value == null) {
+				value = create(key);
+				cache.replace(key, new WeakReference<>(value, queue));
+			}
 		}
-		return ref.get();
+		return value;
 	}
 
 	abstract public V create(K key);
