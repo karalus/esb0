109f161031f8ec340f2bfc4df4f1a22eca168deb
Andre Karalus <andre@karalus-it.de>
2024-12-27 13:33:32 +0100

    Feature: Offer JsonProvider for efficient creation of simple JsonValues

--- src/main/java/com/artofarc/util/JsonFactoryHelper.java
+++ src/main/java/com/artofarc/util/JsonFactoryHelper.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2021 Andre Karalus
+ * Copyright 2024 Andre Karalus
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -17,10 +17,10 @@ package com.artofarc.util;
 
 import java.util.HashMap;
 
-import javax.json.Json;
 import javax.json.JsonBuilderFactory;
 import javax.json.JsonReaderFactory;
 import javax.json.JsonWriterFactory;
+import javax.json.spi.JsonProvider;
 import javax.json.stream.JsonGenerator;
 import javax.json.stream.JsonGeneratorFactory;
 import javax.json.stream.JsonParserFactory;
@@ -29,6 +29,7 @@ import org.glassfish.json.api.BufferPool;
 
 public final class JsonFactoryHelper {
 
+	public static final JsonProvider JSON_PROVIDER = JsonProvider.provider();
 	public static final JsonReaderFactory JSON_READER_FACTORY;
 	public static final JsonParserFactory JSON_PARSER_FACTORY;
 	public static final JsonGeneratorFactory JSON_GENERATOR_FACTORY;
@@ -50,14 +51,14 @@ public final class JsonFactoryHelper {
 				}
 			});
 		}
-		JSON_READER_FACTORY = Json.createReaderFactory(config);
-		JSON_PARSER_FACTORY = Json.createParserFactory(config);
-		JSON_BUILDER_FACTORY = Json.createBuilderFactory(config);
+		JSON_READER_FACTORY = JSON_PROVIDER.createReaderFactory(config);
+		JSON_PARSER_FACTORY = JSON_PROVIDER.createParserFactory(config);
+		JSON_BUILDER_FACTORY = JSON_PROVIDER.createBuilderFactory(config);
 		if (Boolean.parseBoolean(System.getProperty("esb0.jsonPrettyPrinting", "true"))) {
 			config.put(JsonGenerator.PRETTY_PRINTING, true);
 		}
-		JSON_GENERATOR_FACTORY = Json.createGeneratorFactory(config);
-		JSON_WRITER_FACTORY = Json.createWriterFactory(config);
+		JSON_GENERATOR_FACTORY = JSON_PROVIDER.createGeneratorFactory(config);
+		JSON_WRITER_FACTORY = JSON_PROVIDER.createWriterFactory(config);
 	}
 
 }
